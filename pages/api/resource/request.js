b1a975bc4c28b395dc5ef936da0caae84b586fd9
Ashutoshbind15 <91549538+Ashutoshbind15@users.noreply.github.com>
2023-04-16 04:28:40 +0530

    handle resources

--- pages/api/resource/request.js
+++ pages/api/resource/request.js
@@ -1,7 +1,6 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]";
 import connectDB from "../../../utils/db";
-import Resource from "../../../models/Resources";
 import User from "../../../models/User";
 import Business from "../../../models/Business";
 
