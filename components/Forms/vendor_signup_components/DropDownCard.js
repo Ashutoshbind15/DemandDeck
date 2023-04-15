a9e7eb6517b6450221c38bf6a10aeaf165c0a607
Shazeb Khan <shazebk.ug21.cs@nitp.ac.in>
2023-04-15 22:42:00 +0530

    Remove card functionality added

--- components/Forms/vendor_signup_components/DropDownCard.js
+++ components/Forms/vendor_signup_components/DropDownCard.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Close } from '@mui/icons-material'
 
-export default function DropDownCard({text}) {
+export default function DropDownCard({text,onClick}) {
   return (
     <div className='p-4 m-4 ml-0 rounded-md bg-prim shadow-lg text-white flex justify-between'>
         <p className='mr-12'>{text}</p>
-        <button>
+        <button type = "button" onClick = {onClick}>
             <Close/>
         </button>
     </div>
