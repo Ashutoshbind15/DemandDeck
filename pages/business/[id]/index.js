57acb65ce69beea406b79fb8cb3058baff696220
Ashutoshbind15 <91549538+Ashutoshbind15@users.noreply.github.com>
2023-04-16 06:35:02 +0530

    resource fetch on session

--- pages/business/[id]/index.js
+++ pages/business/[id]/index.js
@@ -3,6 +3,7 @@ import { useBusiness } from "../../../hooks/queries";
 import { useRouter } from "next/router";
 import axios from "axios";
 import Business from "../../../components/Business/Business";
+import Resource from "../../../components/Resource/Resource";
 
 const BusinessPage = () => {
   const router = useRouter();
@@ -19,6 +20,28 @@ const BusinessPage = () => {
           Terminate!
         </button> */}
         <Business business={business} />
+        <div className="flex justify-around w-full">
+          <div className="w-1/3 bg-slate-700 text-white rounded-lg px-4 py-4">
+            Requested
+            {business?.resourcesRequested?.map((el) => (
+              <div className="bg-pink-600 my-2 px-2 py-2 rounded-lg">
+                <div>name: {el.resource.title}</div>
+                <div>service: {el.resource.service}</div>
+                <div>from : {el.from.name}</div>
+              </div>
+            ))}
+          </div>
+
+          <div className="w-1/3 bg-slate-700 text-white rounded-lg px-4 py-4">
+            Possessed
+            {business?.resources?.map((el) => (
+              <div className="bg-pink-600 my-2 px-2 py-2 rounded-lg">
+                <div>title: {el.resource.title}</div>
+                <div>service: {el.resource.service}</div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
