de12dd30d22bd40f7fd88c8ca9d6e05d8fb7eb81
Ashutoshbind15 <91549538+Ashutoshbind15@users.noreply.github.com>
2023-04-16 14:14:15 +0530

    add styles

--- components/DemandsPage.js
+++ components/DemandsPage.js
@@ -3,10 +3,10 @@ import { Search, Tune, Favorite } from "../public/icons";
 import DemandCard from "./DemandCard";
 
 export default function DemandsPageComponent({ demands }) {
-  const [searchText,setSearchText] = useState("");
+  const [searchText, setSearchText] = useState("");
 
-  function searchHandler(event){
-    setSearchText(event.target.value)
+  function searchHandler(event) {
+    setSearchText(event.target.value);
   }
 
   return (
@@ -14,14 +14,15 @@ export default function DemandsPageComponent({ demands }) {
       <div class="px-4 flex justify-between items-center shadow-lg">
         <h2 class="text-2xl font-bold">1299 Demands</h2>
         <div class="flex justify-evenly items-center w-1/2">
-        <div className="input-group my-2">
-        <button className= "btn btn-square">
-        {Search}
-        </button>
-        <input type= "text" placeholder= "Search" className="input input-bordered rounded-3xl w-full p-4" 
-          onChange={searchHandler}
-          value = {searchText}
-        />
+          <div className="input-group my-2">
+            <button className="btn btn-square">{Search}</button>
+            <input
+              type="text"
+              placeholder="Search"
+              className="input input-bordered rounded-3xl w-full p-4"
+              onChange={searchHandler}
+              value={searchText}
+            />
           </div>
           {Tune}
         </div>
@@ -37,6 +38,7 @@ export default function DemandsPageComponent({ demands }) {
             key={el._id}
             numresp={el.proposals.length}
             id={el._id}
+            numsup={el.responses.length}
           />
         ))}
       </div>
