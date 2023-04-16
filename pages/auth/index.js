ad361f5b7c96588fcbe4afee4ebf31e00e66989e
Ashutoshbind15 <91549538+Ashutoshbind15@users.noreply.github.com>
2023-04-16 09:29:26 +0530

    fix login

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -3,19 +3,47 @@ import { ErrorMessage, Field, Form, Formik } from "formik";
 import { useSession, signIn } from "next-auth/react";
 import Link from "next/link";
 import React, { useState } from "react";
-import Auth from "../../components/Forms/Auth";
+
 import { useRouter } from "next/router";
 import { authOptions } from "../api/auth/[...nextauth]";
 import { getServerSession } from "next-auth";
 
 const AuthPage = ({ session }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const [signup, setSignup] = useState(true);
 
   const toggleHandler = () => {
     setSignup((prev) => !prev);
   };
 
-  return <Auth signup={signup} setSignup={toggleHandler} />;
+  return (
+    <form>
+      <input
+        type="text"
+        name=""
+        id=""
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        name=""
+        id=""
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button
+        onClick={async (e) => {
+          e.preventDefault();
+          signIn("credentials", { email, password });
+        }}
+      >
+        Submit
+      </button>
+    </form>
+  );
 };
 
 export default AuthPage;
